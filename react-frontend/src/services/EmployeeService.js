6a79a1d1e67f3d60ab6eabce91f84a06197c2074
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-01-27 21:19:16 -0500

    Update EmployeeService.js

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -4,7 +4,6 @@ const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
 
 class EmployeeService {
 
-    
     getEmployees(){
         return axios.get(EMPLOYEE_API_BASE_URL);
     }
